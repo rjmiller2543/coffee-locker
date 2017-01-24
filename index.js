d298bb2a9e976d952b50ee0ce026528586e99a08
RMiller <rmiller@rmiller-mbp.local>
2017-01-24 16:18:47 -0500

    remove all but coffee

--- index.js
+++ index.js
@@ -52,20 +52,6 @@ var router = express.Router();
 
 
 
-  // Create endpoint handlers for /clients
-router.route('/clients')
-  .post(authController.isAuthenticated, clientController.postClients)
-  .get(authController.isAuthenticated, clientController.getClients);
-
-  // Create endpoint handlers for oauth2 authorize
-router.route('/oauth2/authorize')
-  .get(authController.isAuthenticated, oauth2Controller.authorization)
-  .post(authController.isAuthenticated, oauth2Controller.decision);
-
-// Create endpoint handlers for oauth2 token
-router.route('/oauth2/token')
-  .post(authController.isClientAuthenticated, oauth2Controller.token);
-
 // Create endpoint handlers for /coffees
 router.route('/coffees')
   .post(authController.isAuthenticated, coffeeController.postCoffees)
