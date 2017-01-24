4930d90e27ec7e48636efe99048192bb36e0b87c
RMiller <rmiller@rmiller-mbp.local>
2017-01-24 16:22:44 -0500

    removed clients and added authorize

--- index.js
+++ index.js
@@ -55,10 +55,10 @@ router.route('/users')
   .post(userController.postUsers)
   .get(authController.isAuthenticated, userController.getUsers);
 
-  // Create endpoint handlers for /clients
-router.route('/clients')
-.post(authController.isAuthenticated, clientController.postClients)
-.get(authController.isAuthenticated, clientController.getClients);
+  // Create endpoint handlers for oauth2 authorize
+router.route('/oauth2/authorize')
+  .get(authController.isAuthenticated, oauth2Controller.authorization)
+  .post(authController.isAuthenticated, oauth2Controller.decision);
 
 // Create endpoint handlers for /coffees
 router.route('/coffees')
